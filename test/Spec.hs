86df7860bfe94b6cf74fe8888e7aca7cde8b45db
Giovanni Garufi <g.garufi@students.uu.nl>
2017-02-07 19:11:21 +0100

    Added tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,27 @@
+import Parser
+import PrettyPrint
+import System.FilePath.Posix
+import System.Directory
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  cljFiles <- getDirectoryContents "./test" >>= return . map makePath . filter isCljFile
+  mapM_ testFile cljFiles
+  where
+    makePath = (</>) "./test"
+isCljFile :: FilePath -> Bool
+isCljFile f = takeExtension f == ".clj"
+
+parseClj :: String -> [Expr]
+parseClj f = case parse parseTop "test" f of
+  Left err -> error $ show err
+  Right expr -> expr
+
+parseReparse :: String -> Bool
+parseReparse x = (parseClj . ppTop . parseClj) x == parseClj x
+
+testFile :: FilePath -> IO ()
+testFile f = do
+  putStrLn $ "\nTesting file " ++ show f
+  contents <- readFile f
+  putStrLn $ "Parse == Reparse " ++ show (parseReparse contents)
