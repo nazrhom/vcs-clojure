4d05df472ae2682a50e8c6b352132d0d9abddfe6
Victor <v.cacciarimiraldo@gmail.com>
2017-05-15 14:31:20 +0200

    fixed disjoint predicate

--- src/Disjoint.hs
+++ src/Disjoint.hs
@@ -13,21 +13,44 @@ import Lang
 
 
 disjoint :: Almu u v -> Almu u w -> Bool
-disjoint (Alspn s) (Alspn s') = disjointS (disjointAt disjointAlmuH) (disjointAl (disjointAt disjointAlmuH)) s s'
-disjoint (Alins constr ctx) (Alins constr' ctx') = disjointCtx disjointAtmuPos ctx ctx'
+-- * Both are spines, easy
+disjoint (Alspn s) (Alspn s')
+  = disjointS (disjointAt disjointAlmuH)
+              (disjointAl (disjointAt disjointAlmuH)) s s'
+-- * Insertions
+disjoint (Alins constr ctx) almu
+  = disjointFromCtxPos disjoint ctx almu
+disjoint almu (Alins constr ctx)
+  = disjointFromCtxPos disjoint ctx almu
+-- * Deletions and copies are trivially disjoint
+disjoint (Aldel c ctx) (Alspn Scp)
+  = True
+disjoint (Alspn Scp) (Aldel c ctx) 
+  = True
+-- * A patch can never be disjoint from itself.
+disjoint (Aldel c ctx) (Aldel c' ctx')
+  = False 
+disjoint _ _ = False
 
-disjointCtx :: (forall a b . p a -> q b -> Bool)
-          -> Ctx p l1 -> Ctx q l2 -> Bool
-disjointCtx f (Here p l)    (Here p' l') = f p p'
-disjointCtx f (There u ctx) (There u' ctx') = disjointCtx f ctx ctx'
-disjointCtx f a (There u ctx) = disjointCtx f a ctx
-disjointCtx f (There u ctx) b = disjointCtx f ctx b
+
+-- * Deletions and changes are disjoint, if they align properly.
+--   But this is future work
+{-
+disjoint (Aldel c ctx) (Alspn (Scns c' ats))
+  = case testEquality' c c' of
+      Just (Refl , Refl) -> _
+      Nothing            -> False
+-}
+
+
+
+disjointFromCtxPos :: (forall a b . Almu u a -> Almu u b -> Bool)
+                -> Ctx (AtmuPos u) l1 -> Almu u w -> Bool
+disjointFromCtxPos f (Here (FixPos p) l)    almu = f p almu
+disjointFromCtxPos f (There u ctx) almu = disjointFromCtxPos f ctx almu
 
 disjointAlmuH :: AlmuH u -> AlmuH u -> Bool
 disjointAlmuH (AlmuH almu) (AlmuH almu') = disjoint almu almu'
-
-disjointAtmuPos :: AtmuPos u v -> AtmuPos u w -> Bool
-disjointAtmuPos (FixPos almu) (FixPos almu') = disjoint almu almu'
 
 disjointS :: (forall a . at1 a -> at2 a -> Bool)
            -> (forall s d . al1 s d -> al2 s d -> Bool)
@@ -36,33 +59,44 @@ disjointS :: (forall a . at1 a -> at2 a -> Bool)
            -> Bool
 disjointS disAt disAl Scp s'   = True
 disjointS disAt disAl s'  Scp  = True
-disjointS disAt disAl (Scns c p) (Scns c' p') = case testEquality' c c' of
-  Just (Refl, Refl) -> checkAll disAt p p'
-  Nothing -> False
- where
+disjointS disAt disAl (Scns c p) (Scns c' p')
+  = case testEquality' c c' of
+          Just (Refl, Refl) -> checkAll disAt p p'
+          Nothing -> False
+  where
    checkAll :: (forall a . at1 a -> at2 a -> Bool)
             -> All at1 s -> All at2 s -> Bool
    checkAll disAt An An = True
    checkAll disAt (a `Ac` as) (b `Ac` bs) = disAt a b && checkAll disAt as bs
-disjointS disAt disAl (Schg i j p) (Schg i' j' p') = case testEquality' j j' of
-  Just (Refl, Refl) -> case testEquality i i' of
-    Just Refl -> disAl p p'
-    Nothing -> False
-  Nothing -> False
+disjointS disAt disAl (Schg i j p) (Schg i' j' p')
+  = case testEquality' j j' of
+          Just (Refl, Refl) -> case testEquality i i' of
+                                  Just Refl -> disAl p p'
+                                  Nothing -> False
+          Nothing -> False
+disjointS disAt disAl _ _ = False
+
 
 disjointAl :: (forall a . (al1 :: U -> *) a -> (al2 :: U -> *) a -> Bool)
-           -> Al al1 s d -> Al al2 s d -> Bool
+           -> Al al1 s d -> Al al2 s d' -> Bool
 disjointAl disAt A0 A0 = True
-disjointAl disAt (Amod a al) (Amod a' al') = disAt a a' && disjointAl disAt al al'
-disjointAl disAt (Ains a al) (Ains a' al') = disjointAl disAt al al'
-disjointAl disAt (Adel a al) (Adel a' al') = disjointAl disAt al al'
+disjointAl disAt al (Ains a' al') = disjointAl disAt al al'
+disjointAl disAt (Ains a al) al'  = disjointAl disAt al al'
+disjointAl disAt (Adel a al) (Adel a' al')
+  = disjointAl disAt al al'
+disjointAl disAt (Amod a al) (Amod a' al')
+  = disAt a a' && disjointAl disAt al al'
+disjointAl disAt _ _
+  = False
 
 disjointAt :: (IsRecEl a => rec1 a -> rec2 a -> Bool)
            -> At rec1 a -> At rec2 a -> Bool
 disjointAt disjointR (Ai r) (Ai r') = disjointR r r'
-disjointAt disjointR (As p) (As p') = case testEquality new new' of
-  Just Refl -> True
-  Nothing -> False
+disjointAt disjointR (As p) (As p')
+  = case testEquality new new' of
+          Just Refl -> True
+          Nothing -> False
   where
     (old, new) = unContract p
     (old', new') = unContract p'
+
